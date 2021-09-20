20fa8290567effd9258dcf7c60b69a542fcf3ba0
Andrew Spriggs <spriggsy83@yahoo.com.au>
2021-09-20 19:15:42 +1000

    1 Demo Start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <b>React demo</b>
+      <hr />
+      <hr />
+      <p>Hello there</p>
+      <hr />
+      <hr />
+    </div>
+  );
+};
+
+export default App;
